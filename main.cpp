f1b2fa3e1ad63d65229203d5aedc66fc476a8bb5
Guitarist-Tolkienist <guitarist.tolkienist@gmail.com>
2021-01-21 19:53:11 +0200

    - msg from MAx;

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main() {
 	using std::cout;
 	using std::endl;
 	cout << endl << "Finally, we're here!";
+	cout << endl << "Fuck you all, damn fcking people, screw you, dorks!";
 	return EXIT_SUCCESS;
 }
 
