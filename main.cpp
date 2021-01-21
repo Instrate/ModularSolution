d5e572fce35341bf85b1d3bfdda32307fed4a8c0
Guitarist-Tolkienist <guitarist.tolkienist@gmail.com>
2021-01-21 19:59:13 +0200

    - Amazing changes from MAx;

--- main.cpp
+++ main.cpp
@@ -6,10 +6,7 @@ int main() {
 	using std::cout;
 	using std::endl;
 	cout << endl << "Finally, we're here!";
-	cout << endl << "Fuck you all, damn fcking people, screw you, dorks!";
-	cout << endl << "Fuck you all, damn fcking people, screw you, dorks!";
-	cout << endl << "Fuck you all, damn fcking people, screw you, dorks!";
-	cout << endl << "Fuck you all, damn fcking people, screw you, dorks!";
+	cout << endl << "Amazing";
 	return EXIT_SUCCESS;
 }
 
