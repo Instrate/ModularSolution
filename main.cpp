c9ec2a30f7404d4a236a174c2523dc642e84953c
Instrate <Instrate@users.noreply.github.com>
2021-01-22 12:54:53 +0200

    -strarting template

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
-#include <iostream>
-
-
+#include "main.h"
 
 int main() {
-	using std::cout;
-	using std::endl;
-	cout << endl << "Finally, we're here!";
-	cout << endl << "Incredible skuka";
-	cout << endl << "NOT ONLY A Max";
+	
 	return EXIT_SUCCESS;
 }
-
